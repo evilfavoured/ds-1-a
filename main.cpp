62d6520150e8458ec9f947451cf3d55f709d950f
Fatemeh Salehi <fnysalehi@gmail.com>
2016-01-15 23:20:30 +0330

    Fixed a mistake :D

--- main.cpp
+++ main.cpp
@@ -1,116 +1,71 @@
 #include <cstdio>
-#include <string>
 #include <iostream>
-#define MAX_ROW_COUNT 100
-#define MAX_COL_COUNT 100
-
+#include <string>
+#include <vector>
 using namespace std;
 
-int kadane(int* arr, int* start, int* finish, int n)
+#define MAX_INPUT_COUNT 100
+
+struct Node
 {
-	// initialize sum, maxSum and
-	int sum = 0, maxSum = INT_MIN, i;
+	bool is_null;
+	int key;
+	Node *next;
+};
 
-	// Just some initial value to check for all negative values case
-	*finish = -1;
-
-	// local variable
-	int local_start = 0;
-
-	for (i = 0; i < n; ++i)
+// A utility function to print a linked list
+void printList(Node *head)
+{
+	Node* node = head;
+	while (true)
 	{
-		sum += arr[i];
-		if (sum < 0)
-		{
-			sum = 0;
-			local_start = i + 1;
-		}
-		else if (sum > maxSum)
-		{
-			maxSum = sum;
-			*start = local_start;
-			*finish = i;
-		}
+		if (node->is_null)
+			cout << "Null";
+		else
+			cout << node->key;
+		cout << ",";
+		if (node->next == NULL)
+			cout << "Null";
+		else
+			cout << node->next - head;
+		node = node->next;
+		if (node == NULL)
+			break;
+		cout << ";";
 	}
-
-	// There is at-least one non-negative number
-	if (*finish != -1)
-		return maxSum;
-
-	// Special Case: When all numbers in arr[] are negative
-	maxSum = arr[0];
-	*start = *finish = 0;
-
-	// Find the maximum element in array
-	for (i = 1; i < n; i++)
-	{
-		if (arr[i] > maxSum)
-		{
-			maxSum = arr[i];
-			*start = *finish = i;
-		}
-	}
-	return maxSum;
+	cout << endl;
 }
 
-// The main function that finds maximum sum rectangle in M[][]
-void findMaxSum(int** mat, int row_count, int col_count)
+void detectAndRemoveLoop(Node *head)
 {
-	// Variables to store the final output
-	int maxSum = INT_MIN, finalLeft = 0, finalRight = col_count, finalTop = 0, finalBottom = row_count;
+	Node *slow = head;
+	Node *fast = head->next;
 
-	int left, right, i;
-	int sum, start, finish;
-	int* temp = new int[MAX_ROW_COUNT];
-
-	// Set the left column
-	for (left = 0; left < col_count; ++left)
+	// Search for loop using slow and fast pointers
+	while (fast && fast->next)
 	{
-		// Initialize all elements of temp as 0
-		memset(temp, 0, MAX_ROW_COUNT*sizeof(int));
-
-		// Set the right column for the left column set by outer loop
-		for (right = left; right < col_count; ++right)
-		{
-			// Calculate sum between current left and right for every row 'i'
-			for (i = 0; i < row_count; ++i)
-				temp[i] += mat[i][right];
-
-			// Find the maximum sum subarray in temp[]. The kadane() function
-			// also sets values of start and finish.  So 'sum' is sum of
-			// rectangle between (start, left) and (finish, right) which is the
-			//  maximum sum with boundary columns strictly as left and right.
-			sum = kadane(temp, &start, &finish, row_count);
-
-			// Compare sum with maximum sum so far. If sum is more, then update
-			// maxSum and other output values
-			if (sum > maxSum)
-			{
-				maxSum = sum;
-				finalLeft = left;
-				finalRight = right;
-				finalTop = start;
-				finalBottom = finish;
-			}
-		}
+		if (slow == fast)
+			break;
+		slow = slow->next;
+		fast = fast->next->next;
 	}
 
-	delete[] temp;
-
-	for (int row_i = finalTop; row_i <= finalBottom; ++ row_i)
+	/* If loop exists */
+	if (slow == fast)
 	{
-		for (int col_i = finalLeft; col_i <= finalRight; ++col_i)
+		slow = head;
+		while (slow != fast->next)
 		{
-			cout << mat[row_i][col_i];
-			if (col_i + 1 <= finalRight)
-				cout << ",";
+			slow = slow->next;
+			fast = fast->next;
 		}
-		if (row_i + 1 <= finalBottom)
-			cout << ";";
+
+		/* since fast->next is the looping point */
+		fast->next = NULL; /* remove loop */
 	}
 }
 
-// Driver program to test above functions
+/* Drier program to test above function*/
 int main()
 {
 	string raw_input_string;
@@ -121,55 +76,55 @@ int main()
 		return -1;
 	}
 
-	int** mat = new int*[MAX_ROW_COUNT];
-	int mat_row_index = 0;
-	int mat_col_index = 0;
+	Node* nodes = new Node[MAX_INPUT_COUNT];
+	int nodes_index = 0;
 
 	size_t l_off = 0, r_off = 0;
 	while (l_off != string::npos)
 	{
 		r_off = raw_input_string.find_first_of(';', l_off);
 
-		string raw_sub_string = raw_input_string.substr(l_off, r_off == string::npos ? string::npos : r_off - l_off);
-		if (raw_sub_string.empty())
-			break;
+		string node_string = raw_input_string.substr(l_off, r_off == string::npos ? string::npos : r_off - l_off);
+		size_t delimiter_off = node_string.find_first_of(',');
 
-		mat_col_index = 0;
-		int* mat_row = mat[mat_row_index] = new int[MAX_COL_COUNT];
+		string key_string = node_string.substr(0, delimiter_off);
+		string next_string = node_string.substr(delimiter_off + 1, string::npos);
 
-		size_t l_l_off = 0, l_r_off = 0;
-		while (l_l_off != string::npos)
+		Node* node = nodes + nodes_index;
+		try
 		{
-			l_r_off = raw_sub_string.find_first_of(',', l_l_off);
-
-			string node_string = raw_sub_string.substr(l_l_off, l_r_off == string::npos ? string::npos : l_r_off - l_l_off);
-
-			mat_row[mat_col_index] = stoi(node_string);
-
-			l_l_off = l_r_off == string::npos ? string::npos : l_r_off + 1;
-			
-			mat_col_index++;
-			if (mat_col_index >= MAX_COL_COUNT)
-			{
-				cerr << "ERROR: input count limit exceeded" << endl;
-				return -1;
-			}
+			node->key = stoi(key_string);
+			node->is_null = false;
 		}
-		
+		catch(...)
+		{
+			node->key = 0;
+			node->is_null = true;
+		}
+
+		try
+		{
+			int next = stoi(next_string);
+			node->next = nodes + next;
+		}
+		catch (...)
+		{
+			node->next = NULL;
+		}
+
 		l_off = r_off == string::npos ? string::npos : r_off + 1;
-		mat_row_index++;
-		if (mat_row_index >= MAX_ROW_COUNT)
+		nodes_index++;
+		if (nodes_index >= MAX_INPUT_COUNT)
 		{
 			cerr << "ERROR: input count limit exceeded" << endl;
 			return -1;
 		}
 	}
 
-	findMaxSum(mat, mat_row_index, mat_col_index);
+	detectAndRemoveLoop(nodes);
+	printList(nodes);
 
-	for (int i = 0; i < mat_row_index; ++i)
-		delete[] mat[i];
-	delete[] mat;
+	delete[] nodes;
 
 	return 0;
 }
